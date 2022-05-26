cf05c84802eb6efa179016be0b7a4634710e7910
Raphael Marques <raphaeljcmarques@gmail.com>
2022-05-26 20:50:31 -0300

    CHORE: Adding Prismic slice-machine

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,9 +24,9 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return <h1>Home</h1>;
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
